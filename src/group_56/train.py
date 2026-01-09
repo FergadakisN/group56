cbf40547e9bd4aef524108dd9ea5aeaceff15593
Νίκος Φεργαδάκης <nikosfergadakis@gmail.com>
2026-01-09 13:33:37 +0200

    First check commit

---
+++ src/group_56/train.py
@@ -0,0 +1,10 @@
+from group_56.model import Model
+from group_56.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
